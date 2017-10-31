b295e07a9a384b64e4e70fc2d619a5a643bb29b9
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2017-10-31 09:42:00 +0100

    Añadido ejercicio 15 del tema 5

--- Ejercicios Java Tema 5/E15T5.java
+++ Ejercicios Java Tema 5/E15T5.java
@@ -1,11 +1,11 @@
 /**
- * Ejercicio 14: Programa que pide un exponente y una base y te calcula el
- * resultado (solo se pueden introducir numeros positivos).
+ * Ejercicio 15: Programa que pide un exponente y una base y te muestra todas
+ * las potencias hasta llegar a la potencia introducida.
  * 
  * @author Adán
  */
 
-public class E14T5 { // Clase principal
+public class E15T5 { // Clase principal
 public static void main(String[] args) {
   System.out.println("Introduce el exponente (solo numeros positivos)");
   int exponente= Integer.parseInt( System.console().readLine());
@@ -16,10 +16,10 @@ public static void main(String[] args) {
     System.out.println ("El resultado va a ser 1");
    }
   if (exponente > 0) {
-    for (int i = 0; i < exponente; i++){
+    for (int i = 1; i <= exponente; i++){
       potencia = base * potencia;
+      System.out.println(potencia+" es el resultado de "+base+" por "+i+" veces "+base);
     }
   }
-  System.out.println("El resultado de la potencia es: "+potencia);
   }
 }
