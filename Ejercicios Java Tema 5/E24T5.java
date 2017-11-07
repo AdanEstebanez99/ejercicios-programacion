3e8c7af1b35a407b48aeef299b6c069a66c7a71c
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2017-11-07 08:27:19 +0100

    Añadido ejercicio 24

--- Ejercicios Java Tema 5/E24T5.java
+++ Ejercicios Java Tema 5/E24T5.java
@@ -7,7 +7,28 @@
 
 public class E24T5 { // Clase principal
 public static void main(String[] args) {
-
+  System.out.println("Introduce la altura de la piramide:");
+  int alturaIntroducida= Integer.parseInt( System.console().readLine());
+  int altura = 1;
+  int espacio = alturaIntroducida - 1;
+  
+  while (altura <= alturaIntroducida){
+    
+    for (int i = 1; i <= espacio; i++) {
+      System.out.print(" ");
+   }
+    
+    for (int i = 1 ; i <= altura - 1; i++) {
+      System.out.print(i);
+  
+  }
+    for (int i = altura ; i > 0 ; i--) {
+      System.out.print(i);
+    }
+  System.out.println();
+  altura++;
+  espacio--;
+  }
    }
   }
 
