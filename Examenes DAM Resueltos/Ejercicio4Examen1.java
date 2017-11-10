c9b234ca8a534f933698b22aabed13fc7e344ce6
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2017-11-10 15:49:59 +0100

    Arreglado ejercicio 4 examen DAM

--- Examenes DAM Resueltos/Ejercicio4Examen1.java
+++ Examenes DAM Resueltos/Ejercicio4Examen1.java
@@ -10,18 +10,28 @@ System.out.println("Introduce un numero:");
   int numero= Integer.parseInt( System.console().readLine());
   int volteado = 0;
   int volteado2 = 0;
+  boolean comer = true;
   
   while (numero > 0) {
     volteado = ((volteado * 10) + (numero % 10));
     numero /= 10;
     if ((volteado % 10 == 0) || (volteado % 10 == 8)) {
     volteado /= 10;
-    volteado2 = ((numero * 10) + (volteado % 10));
-    volteado /= 10;
+    comer = true;
+    } else {
+      comer = false;
     }
   }
-  System.out.println(volteado2);
-  
+  while (volteado > 0) {
+    volteado2 = ((volteado2 * 10) + (volteado % 10));
+    volteado /= 10;
+  }
+  if (comer = true) {
+  System.out.println("El gusano se ha comido los numeros 8 y 0 y el resultado es: "+volteado2);
+  } 
+  if (comer = false) {
+  System.out.println("El gusano no se ha comido ningun numero "+numero);
+  }
 }
 }
 
