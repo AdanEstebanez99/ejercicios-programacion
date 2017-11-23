1d5f47daed0f78df9dd9c31eec3951ec2a660655
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2017-11-23 09:04:40 +0100

    Arreglado ejercicio 1 del tema 6

--- Ejercicios Java Tema 6/E1T6.java
+++ Ejercicios Java Tema 6/E1T6.java
@@ -4,11 +4,47 @@
  * @author Adán
  */
 
-public class E1T6 { // Clase principal
-public static void main(String[] args) {
-  for (int i = 1 ; i <= 3 ; i++) {
-    int dado = (int)(Math.random()*6) + 1;
-    System.out.println("El dado "+i+" vale: "+dado);
+  public class E1T6 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("Vamos a tirar 3 dados:");
+    int suma = 0;
+    String dadoFigura = "";
+  
+    for (int i = 1 ; i <= 3 ; i ++) {
+      int dado = (int)(Math.random()*6) + 1;
+    
+      switch (dado) {
+      
+        case 1:
+        System.out.print(" ⚀ ");
+        break;
+      
+        case 2:
+        System.out.print(" ⚁ ");
+        break;
+      
+        case 3:
+        System.out.print(" ⚂ ");
+        break;
+      
+        case 4:
+        System.out.print(" ⚃ ");
+        break;
+      
+        case 5:
+        System.out.print(" ⚄ ");
+        break;
+      
+        case 6:
+        System.out.print(" ⚅ ");
+        break;
+        }
+    
+      suma += dado;
+    
+    }
+    System.out.println();
+    System.out.println("La suma de los tres dados es: "+suma);
+  
+    }
   }
-  }
-}
