a980ab02baf9d041a37bc3bb4c8bef5c7cdf7200
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2017-11-24 09:18:06 +0100

    Añadido ejercicio 10 del tema 6

---
+++ Ejercicios Java Tema 6/E10T6.java
@@ -0,0 +1,342 @@
+/**
+ * Ejercicio 10: Programa que pinta 10 lineas con caracteres aleatorios, cada
+ * linea mide entre 1 y 40 caracteres aleatoriamente.
+ * @author Adán
+ */
+
+  public class E10T6 { // Clase principal
+  public static void main(String[] args) {
+    
+    int linea1 = (int)(Math.random()*40)+1;
+    int linea2 = (int)(Math.random()*40)+1;
+    int linea3 = (int)(Math.random()*40)+1;
+    int linea4 = (int)(Math.random()*40)+1;
+    int linea5 = (int)(Math.random()*40)+1;
+    int linea6 = (int)(Math.random()*40)+1;
+    int linea7 = (int)(Math.random()*40)+1;
+    int linea8 = (int)(Math.random()*40)+1;
+    int linea9 = (int)(Math.random()*40)+1;
+    int linea10 = (int)(Math.random()*40)+1;
+    
+    int carAleatorio1 = 0;
+    int carAleatorio2 = 0;
+    int carAleatorio3 = 0;
+    int carAleatorio4 = 0;
+    int carAleatorio5 = 0;
+    int carAleatorio6 = 0;
+    int carAleatorio7 = 0;
+    int carAleatorio8 = 0;
+    int carAleatorio9 = 0;
+    int carAleatorio10 = 0;
+    
+    for (int i = 1 ; i <= linea1 ; i ++) {
+      carAleatorio1 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio1) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea2 ; i ++) {
+      carAleatorio2 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio2) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea3 ; i ++) {
+      carAleatorio3 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio3) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea4 ; i ++) {
+      carAleatorio4 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio4) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea5 ; i ++) {
+      carAleatorio5 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio5) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea6 ; i ++) {
+      carAleatorio6 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio6) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea7 ; i ++) {
+      carAleatorio7 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio7) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea8 ; i ++) {
+      carAleatorio8 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio8) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea9 ; i ++) {
+      carAleatorio9 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio9) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+   
+   for (int i = 1 ; i <= linea10 ; i ++) {
+      carAleatorio10 = (int)(Math.random()*6)+1;
+      
+      switch (carAleatorio10) {
+        case 1:
+        System.out.print("*");
+        break;
+        
+        case 2:
+        System.out.print("-");
+        break;
+        
+        case 3:
+        System.out.print("=");
+        break;
+        
+        case 4:
+        System.out.print(".");
+        break;
+        
+        case 5:
+        System.out.print("|");
+        break;
+        
+        case 6:
+        System.out.print("@");
+        break;
+      }
+   }
+   System.out.println();
+  }
+}
