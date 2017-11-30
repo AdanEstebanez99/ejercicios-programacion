e0cdc085b7e0c4b967b1f1b02872e5c61e0b7ff9
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2017-11-30 15:21:11 +0100

    Corregido ejercicio 14 del tema 6

--- Ejercicios Java Tema 6/E14T6.java
+++ Ejercicios Java Tema 6/E14T6.java
@@ -6,5 +6,42 @@
 
   public class E14T6 { // Clase principal
   public static void main(String[] args) {
+    System.out.println("Piensa un número (entre 0 y 100) e intentaré adivinarlo: ");
+    int i = 1;
+    final int intentos = 8;
+    boolean acertado = false;
+    int maxim = 100;
+    int minim = 0;
+    while((i <= intentos)&&(!acertado)){
+      int num = (int)(Math.random()*(maxim-minim+1))+minim;
+      System.out.println("¿Es el " + num + "?");
+      System.out.println("[s] Sí  |  [+] Tu número es mayor  |  [-] Tu número es menor");
+      String opcion = "";
+      opcion = System.console().readLine().toLowerCase();
+      switch(opcion){
+        case "s":
+          acertado = true;
+          System.out.println("¡Demasiado fácil!");
+          break;
+        case "+":
+          minim = num+1;
+          break;
+        case "-":
+          maxim = num-1;
+          break;
+        default:
+          System.out.println("Comando no disponible");
+          i--;
+          break;
+      }
+      if ((minim > maxim) || (maxim < minim)){
+        System.out.println("No seas tramposo");
+        i--;
+      }
+      i++;
+    }
+    if (!acertado){
+      System.out.println("GG WP");
+    }
   }
 }
