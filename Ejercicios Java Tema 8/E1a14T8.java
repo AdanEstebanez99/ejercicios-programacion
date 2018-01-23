c11417b995a20a97270f21cd41666f7b668e8ce6
Adan Estebanez Villarrubia <adanestebanez@gmail.com>
2018-01-23 08:29:18 +0100

    Añadiendo ejercicios del 1 al 12 de funciones

--- Ejercicios Java Tema 8/E1a14T8.java
+++ Ejercicios Java Tema 8/E1a14T8.java
@@ -27,14 +27,128 @@
     
     //Ejercicio 2:Decir si un numero es o no primo.
     
-    System.out.println("Introduzca un numero entero positivo: ");
-    numero = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduce un número y te diré si es o no primo: ");
+          int numeroIntroducido2 = Integer.parseInt(System.console().readLine());
+          
+          if (Funciones.Varias.esPrimo(numeroIntroducido2)) {
+            System.out.println("El número que has introducido es primo.");
+          } else {
+            System.out.println("El número que has introducido no es primo");
+            }
+    //Ejercicio 3: Decir cual es el siguiente numero primo.
     
-    if (Funciones.Varias.esPrimo(numero)) {
-      System.out.println("El numero "+numero+" es primo");
-    } else {
-      System.out.println("El numero "+numero+" no es primo");
-    }
+    System.out.println("Introduce un número y te dire cuál es el siguiente primo: ");
+    int numeroIntroducido3 = Integer.parseInt(System.console().readLine());
+          
+    int siguiente = Funciones.Varias.siguientePrimo(numeroIntroducido3);
+          
+    System.out.println("El siguiente primo de " + numeroIntroducido3 + " es " + siguiente);
+    
+    //Ejercicio 4: A partir de una base y un exponente se muestra su potencia.
+    
+    System.out.print("Introduce una base: ");
+    int base = Integer.parseInt(System.console().readLine());
+          
+    System.out.print("Ahora Introduce un exponente: ");
+    int exponente = Integer.parseInt(System.console().readLine());
+          
+    int potencia = Funciones.Varias.potencia(base, exponente);
+          
+    System.out.println("La potencia de " + base + " elevado a " + exponente + " es " + potencia);
+    
+    //Ejercicio 5: Cuenta los digitos de un numero introducido por teclado.
+    
+    System.out.print("Introduce un número y te diré cuantos dígitos tiene: ");
+    int numeroIntroducido4 = Integer.parseInt(System.console().readLine());
+           
+    int cuentaDigito = Funciones.Varias.digitos(numeroIntroducido4);
+           
+    System.out.println("El " + numeroIntroducido4 + " tiene " + cuentaDigito + " dígitos");
+    
+    //Ejercicio 6: Voltea un numero introducido por teclado.
+    
+    System.out.print("Introducé un número y te lo voltearé: ");
+    int numeroIntroducido5 = Integer.parseInt(System.console().readLine());
+           
+    int volteado = Funciones.Varias.voltea(numeroIntroducido5);
+           
+    System.out.println("Tu número volteado es " + volteado);
+    
+    //Ejercicio 7: Muestra la posicion de un digito introducido por el ususario.
+    
+    System.out.print("Introduce un número: ");
+    int numeroIntroducido6 = Integer.parseInt(System.console().readLine());
+           
+    System.out.print("Introduce un numero y te dire el digito de esa posicion: ");
+    int posicion = Integer.parseInt(System.console().readLine());
+           
+    int posicionDelDigito = Funciones.Varias.digitoN(numeroIntroducido6, posicion);
+           
+    System.out.println("En esa posición está el " + posicionDelDigito);
+    
+    //Ejercicio 8: Programa que dice la ocurrencia de un numero:
+    
+    System.out.print("Introduce un número: ");
+    int numeroIntroducido7 = Integer.parseInt(System.console().readLine());
+           
+    System.out.print("Ahora introduce un número de 1 cifra: ");
+    int cifra = Integer.parseInt(System.console().readLine());
+           
+    int ocurrencia = Funciones.Varias.posicionDeDigito(numeroIntroducido7, cifra);
+           
+    if(ocurrencia == -1){
+      System.out.println("El número " + cifra + " no se encuentra " + ocurrencia);
+      } else {
+        System.out.println("El número " + cifra + " aparece primero en la posición " + ocurrencia);
+      }
+      
+    //Ejercicio 9: Programa que quita n digitos a un numero por detras.
+    
+    System.out.print("Introduce un número largo: ");
+    long numeroIntroducido8 = Long.parseLong(System.console().readLine());
+           
+    System.out.print("¿Cuántos dígitos le quieres quitar al número por detrás?: ");
+    long quitarDetras = Long.parseLong(System.console().readLine());
+           
+    long resultadoDetras = Funciones.Varias.quitarPorDetras(numeroIntroducido8, quitarDetras);
+           
+    System.out.println("Este es tu número acortado: " + resultadoDetras);
+    
+    //Ejercicio 10: Programa que quita n digitos a un numero por delante.
+    
+    System.out.print("Introduce un número largo: ");
+    int numeroIntroducido9 = Integer.parseInt(System.console().readLine());
+           
+    System.out.print("¿Cuántos dígitos le quieres quitar al número por delante?: ");
+    int quitarDelante = Integer.parseInt(System.console().readLine());
+           
+    int resultadoDelante = Funciones.Varias.quitarPorDelante(numeroIntroducido9, quitarDelante);
+           
+    System.out.println("Este es tu número acortado: " + resultadoDelante);
+    
+    //Ejercicio 11: Programa que le añade digitos por detras a un numero.
+    
+    System.out.print("Introduce un número largo: ");
+    int numeroIntroducido10 = Integer.parseInt(System.console().readLine());
+           
+    System.out.print("Escribe un dígito para pegarselo por detrás al número: ");
+    int pegarDetras = Integer.parseInt(System.console().readLine());
+           
+    int resultadoPegaDetras = Funciones.Varias.pegaPorDetras(numeroIntroducido10, pegarDetras);
+           
+    System.out.println("Este es tu número con el dígito añadido: " + resultadoPegaDetras);
+    
+    //Ejercicio 12: Programa que le añade digitos por detras a un numero.
+    
+    System.out.print("Introduce un número largo: ");
+    int numeroIntroducido11 = Integer.parseInt(System.console().readLine());
+           
+    System.out.print("Escribe un dígito para pegarselo por delante al número: ");
+    int pegarDelante = Integer.parseInt(System.console().readLine());
+           
+    int resultadoPegaDelante = Funciones.Varias.pegaPorDelante(numeroIntroducido11, pegarDelante);
+           
+    System.out.println("Este es tu número con el dígito añadido: " + resultadoPegaDelante);
   }
 }
 
--- Ejercicios Java Tema 8/Funciones/Varias.java
+++ Ejercicios Java Tema 8/Funciones/Varias.java
@@ -21,17 +21,133 @@
   //Numero capicua:
   
     public static boolean esCapicua(int x) {
-      int numeroReves = 0;
-      int numero = x;
-      while (numero > 0) {
-        numeroReves = (numeroReves * 10) + (numero % 10);
-        numero /= 10;
+      return x == voltea(x);
       }
-      if (numero == numeroReves){
-          return true;
+      
+  //Siguiente numero primo:
+    
+    public static int siguientePrimo(int n){
+      int nextPrimo = n;
+      
+      do{
+        nextPrimo++;
+      }while(!esPrimo(nextPrimo));
+      return nextPrimo;
+      }
+  
+  //Segun base y exponente devolver la potencia:
+    
+    public static int potencia(int b, int e){
+     int potencia = 1;
+     
+     for (int i = 0; i < e; i++){
+       potencia = potencia * b;
+     }
+     
+     return potencia;
+     }
+    
+  //Contador de Digitos:
+  
+    public static int digitos(int n){
+      int digitos = 1;
+      
+      while(n > 9) {
+        n = n / 10;
+        digitos++;
+      }
+      return digitos;
+      }
+      
+  //Voltea el numero:
+  
+    public static int voltea(int n){
+     int volteado = 0;
+     
+     while(n > 0){
+       volteado = volteado * 10 + n % 10;
+       n = n / 10;
+     }
+     
+     return volteado;
+     }
+     
+  //Devuelve el digito en el que esta la posicion de un numero entero:
+  
+    public static int digitoN(int n, int x){
+      int aux = voltea(n);
+      
+      if (x == 1){
+        return aux % 10;
+      }else{
+        for(int i = 0; i < x; i++){
+          aux = aux / 10;
         }
-     return false;
+        
+        return aux % 10;
+      }
+      }
+      
+  //Posicion de la primera ocurrencia de un digito:
+  
+    public static int posicionDeDigito(int numero, int digito) {
+       int volteado = voltea(numero);
+       int i = 0;
+       while (volteado >= 1) {
+         if (digito == volteado % 10) {
+          return i;
+         }
+         volteado = volteado/10;
+         i++;
+       }
+       return -1;
+       
+      }
+      
+  //Quita n digitos a un numero por detras:
+  
+    public static long quitarPorDetras(long n, long x) {
+       int contador = 0;
+       
+       while(contador < x){
+         n = n/10;
+         contador++;
+       }
+       return n;
+      }
+      
+  //Quita n digitos a un numero por delante:
+  
+    public static int quitarPorDelante(int n, int x) {
+      n = voltea(n);
+      int contador = 0;
+
+      while(contador < x){
+        
+        n = n/10;
+        contador++;
+      }
+      n = voltea(n);
+      return n;
     }
     
+  //Añade un digito a un numero por detras:
+  
+    public static int pegaPorDetras(int n, int x) {
+     n = n*10;
+     n = n + x;
     
+     return n;
+    }
+    
+  //Añade un digito a un numero por delante:
+  
+    public static int pegaPorDelante(int n, int x) {
+     n = voltea(n);
+     
+     n = n*10;
+     n = n + x;
+     
+     return n = voltea(n);
+    }
 }
