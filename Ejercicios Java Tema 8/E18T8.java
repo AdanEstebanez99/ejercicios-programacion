4f5b4e9560fe2229a62a43a90253bd6951e06aef
unknown <adanestebanez@gmail.com>
2018-02-16 08:37:08 +0100

    Añadido ejercicio 18 de funciones

---
+++ Ejercicios Java Tema 8/E18T8.java
@@ -0,0 +1,34 @@
+/*
+ *Ejercicio 17 del tema 8: Conversor de numeros decimales a numeros binarios.
+ *
+ * @author Adan
+ */
+package ejercicio17tema8;
+
+import java.util.Scanner;
+
+public class E18T8 {
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Introduzca un número decimal para pasarlo a binario: ");
+        int decimal = s.nextInt();
+        System.out.println("El resultado en binario es " + decimalBinario(decimal));
+    }
+    public static long decimalBinario(int decimal) {
+        if (decimal == 0) {
+            return 0;
+        }
+        long binario = 1;
+        
+        while (decimal > 1) {
+            binario = Funciones.Varias.pegaPorDetras(binario, decimal % 2);
+            decimal = decimal / 2;
+        }
+        binario = Funciones.Varias.pegaPorDetras(binario, 1);
+        binario = Funciones.Varias.voltea(binario);
+        binario = Funciones.Varias.quitaPorDetras(binario, 1);
+        return binario;
+
+  }
+}
